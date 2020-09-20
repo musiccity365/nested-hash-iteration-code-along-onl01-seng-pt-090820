dd63cbb45cc793ed98f1108b2b1e0eabc1fd6a76
Michael David Sackstein <m_sackstein@yahoo.com>
2020-09-20 23:43:30 +0000

    Automatically backed up by Learn

--- lib/contacts.rb
+++ lib/contacts.rb
@@ -21,7 +21,7 @@ def remove_strawberry(contacts)
       contact_details_hash.each do |attribute, data|
         # binding.pry
         if attribute == :favorite_ice_cream_flavors
-          binding.pry
+          # binding.pry
         end
       end
     end
