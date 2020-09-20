18310e4ef94023455d083d2b852c626e6fd10e6e
Michael David Sackstein <m_sackstein@yahoo.com>
2020-09-20 23:43:06 +0000

    Automatically backed up by Learn

--- lib/contacts.rb
+++ lib/contacts.rb
@@ -20,7 +20,9 @@ def remove_strawberry(contacts)
     if person == "Freddy Mercury"
       contact_details_hash.each do |attribute, data|
         # binding.pry
-        
+        if attribute == :favorite_ice_cream_flavors
+          binding.pry
+        end
       end
     end
   end
